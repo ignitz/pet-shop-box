f43c6d26deba1db5ff936c0c2d3ecfb08fc1afaf
Luis Philipe Fidelis <luizsale10@gmail.com>
2018-02-17 01:37:16 -0200

    step2

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,36 +2,50 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
     
-    Pet[] pets;
+    Pet[] public pets;
 
     struct Pet {
         bytes name;
         address adopter;
     }
 
+    modifier validPet(uint petId) {
+        require(petId >= 0 && petId < pets.length);
+        _;
+    }
+
+    // adding a pet
     function addPet(bytes name) public returns(uint) {
         Pet memory newPet = Pet(name, address(0));
         pets.push(newPet);
         return pets.length - 1;
     }
 
+    // Retrieving number of pets
+    function getNumberOfPets() public view returns (uint) {
+        return pets.length;
+    }
+
+     // Retrieving a pet
+    function getPet(uint petId) 
+        validPet(petId)
+        public 
+        view 
+        returns(bytes, address) 
+    {
+        return (pets[petId].name, pets[petId].adopter);
+    }
+
     // Adopting a pet
-    function adopt(uint petId) public returns (uint) {
-        require(petId >= 0 && petId < pets.length);
+    function adopt(uint petId) 
+        validPet(petId)
+        public 
+        returns (uint) 
+    {
         Pet storage pet = pets[petId];
         pet.adopter = msg.sender;
         pets[petId] = pet;
         return petId;
-    }
-
-    // Retrieving a pet
-    function getPet(uint petId) public view returns(bytes, address) {
-        return (pets[petId].name, pets[petId].adopter);
-    }
-
-    // Retrieving number of pets
-    function getNumberOfPets() public view returns (uint) {
-        return pets.length;
     }
 
 }
--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -3,45 +3,57 @@ pragma solidity ^0.4.17;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
+import "./ThrowProxy.sol";
 
 contract TestAdoption {
+    
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
+    ThrowProxy throwProxy = new ThrowProxy(address(adoption)); 
+    Adoption throwableAdoption = Adoption(address(throwProxy));
     
     // Testing the addPet() function
     function testUserCanAddPet() public {
         uint returnedId = adoption.addPet("Dog Test");
-
         uint expected = 0;
-
         Assert.equal(returnedId, expected, "pet ID 0 should be recorded.");
+    }
+
+    // Testing the getNumberOfPets() function
+    function testUserCanGetTheNumberOfPets () public {
+        uint expectedPetLength = 1;
+        uint petLength = adoption.getNumberOfPets();
+        Assert.equal(petLength, expectedPetLength, "Number of pets should be 1.");
+    }
+
+    // Testing the getPet() function
+    function testUserCanGetPet() public {
+        // Expected adopter is null
+        address expectedAdopter = address(0);
+        var (,adopter) = adoption.getPet(0);
+        Assert.equal(adopter, expectedAdopter, "Adopter of pet ID 0 should be empty.");
     }
 
     // Testing the adopt() function
     function testUserCanAdoptPet() public {
         uint returnedId = adoption.adopt(0);
-
-        uint expected = 0;
-
-        Assert.equal(returnedId, expected, "Adoption of pet ID 0 should be recorded.");
-    }
-
-    // Testing retrieval of a pet
-    function testGetPetByPetId() public {
-        // Expected owner is this contract
-        address expected = this;
-
+        uint expectedId = 0;
+        address expectedAdopter = this;
+        Assert.equal(returnedId, expectedId, "Adoption of pet ID 0 should be recorded.");
         var (,adopter) = adoption.getPet(0);
-
-        Assert.equal(adopter, expected, "Owner of pet ID 0 should be recorded.");
+        Assert.equal(adopter, expectedAdopter, "Adopter of pet ID 0 should be the TestAdoption contract.");
     }
 
-    // Testing the getPetLength() function
-    function testGetPetLength() public {
-        uint expectedPetLength = 1;
+    // Testing invalid retrieval of a pet
+    function testUserCannotGetAnInvalidPet() public {
+        throwableAdoption.getPet(1);
+        throwProxy.shouldThrow();
+     }
 
-        uint petLength = adoption.getPetLength();
-        
-        Assert.equal(petLength, expectedPetLength, "Number of pets should be 1.");
+    // Testing invalid adoption 
+    function testUserCannotAdoptAnInvalidPet() public {
+        throwableAdoption.adopt(1);
+        throwProxy.shouldThrow();
     }
+     
     
 }
